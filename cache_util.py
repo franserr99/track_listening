8910d3f1f7c57e51805687ac570e6b1ae2d12b44
francisco laptop <serrfrancisco@yahoo.com>
2023-01-22 12:49:06 -0800

    more progress on sending data to a db

--- cache_util.py
+++ cache_util.py
@@ -1,29 +1,46 @@
-import json,sys
+import json,sys, requests, base64, os, datetime
+from datetime import datetime
+from dotenv import load_dotenv
 
 def get_cached_token():
-    #check if there is a cached token, if there is then let this process happen, else hit for a new token
-    #might also be where you check for scopes and the like 
-
+    cache_data=None
     with open('.cache', 'r') as cache_file:
-        # Read the contents of the file: is stored as a dictionary
-
-        #check if a refresh is needed here
-        cache_data = cache_file.read()
-        dict=json.loads(cache_data)
-        return dict
-def update_cache(data:dict=None):
-    if not data: #no dict passed
-        data = get_cached_token()
-        # Open the .cache file for writing
-        with open('.cache', 'w') as cache_file:
-            # Write the data to the file
-            cache_file.write(json.dumps(data))
-    else: 
-        with open('.cache', 'w') as cache_file:
-            # Write the data to the file
-            cache_file.write(json.dumps(data))
+        # read the contents of the file: is stored as a dictionary
+        cache_data = json.loads(cache_file.read())
+    now=datetime.now()
+    if cache_data:
+        expiration_date=datetime.fromtimestamp(cache_data['expires_at'])
+        if(expiration_date<now):
+            refresh_token() #make it return something, then reassign cache_date reference 
+        return cache_data
+    else:
+        #no cached token
+        pass
+def refresh_token():
+    if ('SPOTIPY_CLIENT_ID' or 'SPOTIPY_CLIENT_SECRET') not in os.environ :
+        load_dotenv("/Users/franserr/Documents/portfolio/spotify_usage/env_files/client.env")    
+    try: 
+        client_secret=os.environ["SPOTIPY_CLIENT_SECRET"] 
+        client_ID=os.environ['SPOTIPY_CLIENT_ID']
+    except:
+        print("one of these were not set as an enviormental variable, needed for successful execution")
+        sys.exit(1) 
+    client_ID=base64.b64encode(client_ID.encode())
+    client_secret=base64.b64encode(client_secret.encode())
+    auth_info= get_cached_token()
+    refresh_token=auth_info['refresh_token']
+    headers = {'Authorization':'Basic '+ client_ID+':'+ client_secret , 'Content-Type': 'application/x-www-form-urlencoded'}
+    data = 'grant_type='+refresh_token+'&'+refresh_token+'='+refresh_token+''
+    url= 'https://accounts.spotify.com/api/token'
+    response= requests.post(url=url,headers=headers, data=data ).json()
+    if'expires_at' not in response:
+        expires_in=response['expires_in']
+        #n seconds from now
+        expiration_date=datetime.now()+ datetime.timedelta(seconds=expires_in)
+        #now create a timestamp 
+        response['expires_at']=int(expiration_date.timestamp())
+    update_cache(response)
 def get_auth_info(cached_token:dict=None, refresh_token:bool=False):
-    #token_info=[]
     if cached_token==None:
         token =get_cached_token()
         if(refresh_token):
@@ -35,23 +52,30 @@ def get_auth_info(cached_token:dict=None, refresh_token:bool=False):
             return cached_token['token_type'], cached_token['access_token'], cached_token['refresh_token']
         else:
             return cached_token['token_type'], cached_token['access_token']
-        
-#creating an object out of the client is better, the way spotipy is set up they have it built into the function calls for checking 
 def get(key):
     cache = get_cached_token()
     if key in cache:
         return cache[key]
     else:
         return None
-    
 def set(key, value):
     cache = get_cached_token()
     cache[key] = value
     update_cache(cache)
-    
-    
 def delete(key):
     cache = get_cached_token()
     if key in cache:
         del cache[key]
     update_cache(cache)
+def update_cache(data:dict=None):
+    if not data: #no dict passed
+        data = get_cached_token()
+        # Open the .cache file for writing
+        with open('.cache', 'w') as cache_file:
+            # Write the data to the file
+            cache_file.write(json.dumps(data))
+    else: 
+        with open('.cache', 'w') as cache_file:
+            # Write the data to the file
+            cache_file.write(json.dumps(data))
+#now test the refresh method 
--- get_top_tracks.py
+++ get_top_tracks.py
@@ -1,19 +1,28 @@
-import os, sys, time, spotipy, requests, cache_util
+import os, sys, time, spotipy, requests, cache_util, datetime, pytz
 from cache_util import get_cached_token
 from  dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyOAuth
+import sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 def main(): 
-
-    #the concepts of 
-
     load_dotenv("/Users/franserr/Documents/portfolio/spotify_usage/env_files/client.env")     #load env vars
+    load_dotenv("/Users/franserr/Documents/portfolio/spotify_usage/env_files/db.env")
     scope= "user-library-read user-read-playback-position user-top-read user-read-recently-played playlist-read-private"
+    #now= datetime.datetime.now(tz=pytz.timezone('US/Pacific'))
+    #now=now.strftime('%Y-%m-%d %H:%M:%S')
+
+
     try: 
         #load_dotenv should load env var, now just check they exist in the enviorment
         os.environ["SPOTIPY_CLIENT_SECRET"] 
         os.environ['SPOTIPY_CLIENT_ID']
         os.environ["SPOTIPY_REDIRECT_URI"]
+        os.environ['user']
+        os.environ['password']
+        os.environ['port']
     except:
         print("one of these were not set as an enviormental variable, needed for successful execution")
         sys.exit(1) #use this instead of exit() bc it speaks to interpreter, not safe in prod env
@@ -22,45 +31,76 @@ def main():
     oauth=SpotifyOAuth(scope=scope)
     sp=spotipy.Spotify(auth_manager=oauth)
     get_top_tracks(sp)
-
-
-
-
     
 def get_top_tracks(client:spotipy.Spotify ): 
-
-    tracks= []
+    #get the idx w corresponding track name & artist for each near-term
     short_term_tracks=client.current_user_top_tracks(20,offset=0,time_range= "short_term" )
+    #init near-term meta data, process data for each term
     st_track_idx,st_tracks_name, st_artist_names=get_tracks_info(short_term_tracks)
     if(short_term_tracks['next']): 
         paginate_results(short_term_tracks, st_track_idx, st_tracks_name, st_artist_names)
-    print(short_term_tracks)
-    print(st_track_idx)
-    print(st_tracks_name)
-    print(st_artist_names)
-    sys.exit(1)
-
     middle_term_tracks=client.current_user_top_tracks(20,offset=0,time_range= "middle_term" )
     mt_track_idx,mt_tracks_name, mt_artist_names=get_tracks_info(middle_term_tracks)
     if(middle_term_tracks['next']): 
         paginate_results(middle_term_tracks, mt_track_idx, mt_tracks_name, mt_artist_names)
     long_term_tracks=client.current_user_top_tracks(20,offset=0,time_range="long_term" )
-
     lt_track_idx,lt_tracks_name, lt_artist_names=get_tracks_info(long_term_tracks)
     if(long_term_tracks['next']): 
         paginate_results(long_term_tracks, lt_track_idx, lt_tracks_name, lt_artist_names)
+    #get unique track, use a set
+    unique_track_idx=set()
+    #unique in context of for each unique id
+    unique_track_artists=[] 
+    unique_track_names=[]
+    #init the iterables w/ short-term
+    unique_track_idx.update(st_track_idx)
+    unique_track_artists.extend(st_artist_names)
+    unique_track_names.extend(st_tracks_name)
+    #middle-term, then longterm
+    for track, i in enumerate(mt_track_idx):
+        if track not in unique_track_idx:
+            unique_track_idx.add(track)
+            unique_track_artists.append(mt_artist_names[i])
+            unique_track_names.append(mt_tracks_name[i])
+    for track, i in enumerate(lt_track_idx):
+        if track not in unique_track_idx:
+            unique_track_idx.add(track)
+            unique_track_artists.append(lt_artist_names[i])
+            unique_track_names.append(lt_tracks_name[i])
+    assert(len(unique_track_idx)==len(unique_track_artists)==len(unique_track_names))
 
+    now= datetime.datetime.now(pytz.timezone('US/Pacific')).strftime('%Y-%m-%d %H:%M:%S')
+    #part one: done
+    #part two: connect to the my database and store it
+
+
+    user=os.environ['user']
+    pw=os.environ['password']
+    port=os.environ['port']
+    connector_str='mysql+mysqlconnector://'+user+':'+ pw+ '@localhost:'+ port+'/test_DB'
+    engine=create_engine(connector_str)
+
+    #check to see if the database contains the spotify id of the client 
+    # if not 
+    
+
+
+
+
+
+
+
+
+    
 
 def paginate_results(tracks:dict, idx:list, track_names:list, artist_names:list):
     type, access_token = cache_util.get_auth_info()
-
     while(tracks['next']):
         tracks= requests.get(tracks['next'], headers={ 'Authorization': type+" "+access_token }).json()
         more_idx, more_track_names,more_artist_names= get_tracks_info(tracks)
         idx.extend(more_idx)
         track_names.extend(more_track_names)
         artist_names.extend(more_artist_names)
-    
 def get_tracks_info(tracks:dict):
     tracks_URI=[]
     tracks_name=[]
@@ -77,7 +117,5 @@ def get_tracks_info(tracks:dict):
         artists_name.append(artists) #get first artist associated with the song in the json obj
     assert(len(tracks_URI)==len(tracks_name)==len(artists_name))
     return tracks_URI, tracks_name, artists_name
-    
-    
 main()
 
